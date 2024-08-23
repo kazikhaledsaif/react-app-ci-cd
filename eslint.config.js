6e6fd16220cdb71eff73d4bd9c4c649cf7b5f910
Kazi Khaled Saif <kksaif05@gmail.com>
2024-08-23 14:48:33 +0900

    feat: added prittter and precommit

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist'],
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -22,13 +25,16 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier, // Include the prettier plugin here
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
       'react/jsx-no-target-blank': 'off',
+      'prettier/prettier': 'error', // Ensure this rule is correctly set
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
